7cfa9a9e3a6eff3d1995d4f215b3a98a58da91d2
mathe <mathe@DESKTOP-3O5QJKT>
2025-01-30 16:53:30 +0100

    Implémentation des méthodes de Coordonnée

--- src/util/Coordonnee.java
+++ src/util/Coordonnee.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package util;
 
 /**
@@ -9,4 +6,32 @@ package util;
 public class Coordonnee {
 	private int x;
 	private int y;
+	
+	public Coordonnee(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;	
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public boolean equals(Coordonnee coordonnee) {
+		if (this.x == coordonnee.getX() && this.y == coordonnee.getY()) {
+			return true;
+		}
+		return false;
+	}
 }
