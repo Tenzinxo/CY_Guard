1ce992735f73c61357bf50efbcae21cfb163d6c1
mathe <mathe@DESKTOP-3O5QJKT>
2025-01-30 17:24:45 +0100

    Représentation d'un Obastacle et d'une Case

--- src/environnement/Case.java
+++ src/environnement/Case.java
@@ -7,6 +7,27 @@ import util.Coordonnee;
  * Représente une case de la grille.
  */
 public class Case {
-	private Coordonnee coordonnee;
+	private Coordonnee position;
 	private Obstacle obstacle;
+	
+	public Case(Coordonnee position, Obstacle obstacle) {
+		this.position = position;
+		this.obstacle = obstacle;
+	}
+	
+	public Coordonnee getPosition() {
+		return this.position;
+	}
+	
+	public void setPosition(Coordonnee position) {
+		this.position = position;
+	}
+	
+	public Obstacle getObstacle() {
+		return this.obstacle;
+	}
+	
+	public void setObstacle(Obstacle obstacle) {
+		this.obstacle = obstacle;
+	}
 }
--- src/environnement/obstacle/Obstacle.java
+++ src/environnement/obstacle/Obstacle.java
@@ -1,5 +1,25 @@
 package environnement.obstacle;
 
-public class Obstacle {
+public abstract class Obstacle {
+	private String type;
+	private boolean BloqueVision;
+	private boolean BloqueDeplacement;
+	
+	public Obstacle(String type, boolean isBloqueVision, boolean isBloqueDeplacement) {
+		this.type = type;
+		this.BloqueVision = isBloqueVision;
+		this.BloqueDeplacement = isBloqueDeplacement;
+	}
 
+	public String getType() {
+		return type;
+	}
+
+	public boolean isBloqueVision() {
+		return BloqueVision;
+	}
+
+	public boolean isBloqueDeplacement() {
+		return BloqueDeplacement;
+	}
 }
