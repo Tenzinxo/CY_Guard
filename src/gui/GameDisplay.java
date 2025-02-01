dfd120658eb5062dc4debc62c1243474fcb0938c
existenzin <rigsangzurkhang@gmail.com>
2025-02-01 17:08:43 +0100

    ajout de GameDisplay et keyControl

--- src/gui/GameDisplay.java
+++ src/gui/GameDisplay.java
@@ -1,5 +1,20 @@
 package gui;
 
-public class GameDisplay {
+import java.awt.Graphics;
 
+import javax.swing.JPanel;
+
+import environnement.Grille;
+
+public class GameDisplay extends JPanel{
+	
+	private Grille grille;
+	
+	public GameDisplay(Grille grille) {
+		this.grille = grille;
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+	}
 }
--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,6 +1,77 @@
 package gui;
 
-public class MainGUI {
+import javax.swing.JFrame;
 
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import config.GameConfiguration;
+import environnement.Grille;
+
+public class MainGUI extends JFrame implements Runnable{
+	
+	private static Dimension preferredsize = new Dimension(GameConfiguration.WINDOW_WIDTH,GameConfiguration.WINDOW_HEIGHT);
+	
+	
+	private Grille grille;
+	
+	
+
+	public MainGUI(String title) throws HeadlessException {
+		super(title);
+		init();
+		// TODO Auto-generated constructor stub
+	}
+
+	private void init() {
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class KeyControls implements KeyListener{
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			char keychar = e.getKeyChar();
+			switch (keychar) {
+			
+			case 'a':
+				//manager.moveleftGardien;
+				break;
+			
+			case 'd':
+				//manager.moverightGardien;
+				break;
+				
+			case 'w':
+				//manager.moveupGardien;
+				break;
+				
+			case 's':
+				//manager.movedownGardien;
+				break;
+			default:
+				break;
+			}
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			
+		}
+
+	}
 }
