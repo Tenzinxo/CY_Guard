2f5f00db2e4ded311945533c414f264afb7b3ab2
Matheo <Matheo@GAMING>
2025-02-01 16:03:12 +0100

    Ajout des parametres du jeux.

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -2,8 +2,23 @@ package config;
 
 public class GameConfiguration {
 	
-	public static final int INTRUS_SPEED = 10;
-	public static final int GARDIENS_SPEED = 12;
+	public static final int VITESSE_INTRUS = 10;
+	public static final int VITESSE_GARDIENS = 12;
 	
-	public static final boolean ALLOW_DIAGONAL_DISPLACEMENT = true;
+	public static final int DIFFICULTE = 1;
+	
+	public static final int NB_LIGNE = 25;
+	public static final int NB_COLONNE = 25;
+	
+	public static final int DENSITE_ROCHE = 20;
+	public static final int NB_ROCHE = (NB_LIGNE*NB_COLONNE)/(8*DIFFICULTE);
+	
+	public static final int DENSITE_ARBRE = 5;
+	public static final int NB_ARBRE = (NB_LIGNE*NB_COLONNE)/(10*DIFFICULTE);	
+	
+	public static final int DENSITE_LAC = 40;
+	public static final int NB_LAC = (NB_LIGNE*NB_COLONNE)/(6*DIFFICULTE);	
+	
+	
+	public static final boolean PERMET_DEPLACEMENT_DIAGONNAL = true;
 }
