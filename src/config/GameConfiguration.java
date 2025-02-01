41fb92c0703619e76cc4af840764cf792f9f9e45
Matheo <Matheo@GAMING>
2025-02-01 14:57:41 +0100

    config pour le déplacement diagonal

--- src/config/GameConfiguration.java
+++ src/config/GameConfiguration.java
@@ -4,4 +4,6 @@ public class GameConfiguration {
 	
 	public static final int INTRUS_SPEED = 10;
 	public static final int GARDIENS_SPEED = 12;
+	
+	public static final boolean ALLOW_DIAGONAL_DISPLACEMENT = true;
 }
